74d60ada674f78b1fd6a9cfcd1bf22797fc7004c
Your Name <sangameshshishir@gmail.com>
2024-08-30 18:07:53 +0530

    Updated code

--- src/ListOfInt.java
+++ src/ListOfInt.java
@@ -1,43 +1,66 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListOfInt {
+
+    static void addIfEven(int num, List<Integer> evenNumbers) {
+        if (num % 2 == 0) {
+            evenNumbers.add(num);
+        }
+    }
+
+    static List<Integer> getEvenNumForEach(List<Integer> intNum) {
+        List<Integer> evenNumbers = new ArrayList<>();
+        for (int num : intNum) {
+            addIfEven(num, evenNumbers);
+        }
+        return evenNumbers;
+    }
+
+    static List<Integer> getEvenNumWhile(List<Integer> intNum) {
+        List<Integer> evenNumbers = new ArrayList<>();
+        int i = 0;
+        while (i < intNum.size()) {
+            int num = intNum.get(i);
+            addIfEven(num, evenNumbers);
+            i++;
+        }
+        return evenNumbers;
+    }
+
+    static List<Integer> getEvenNumFor(List<Integer> intNum) {
+        List<Integer> evenNumbers = new ArrayList<>();
+        for (int j = 0; j < intNum.size(); j++) {
+            int num = intNum.get(j);
+            addIfEven(num, evenNumbers);
+        }
+        return evenNumbers;
+    }
+
+    static List<Integer> getEvenNumStream(List<Integer> intNum) {
+        return intNum.stream()
+                .filter(num -> num % 2 == 0)
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         List<Integer> intNum = new ArrayList<>();
 
-        for (int i=1; i<=10; i++) {
+        for (int i = 1; i <= 10; i++) {
             intNum.add(i);
         }
 
-        System.out.println("Even numbers in the list using for each loop : ");
-        for (int num : intNum) {
-            if (num % 2 == 0) {
-                System.out.println(num);
-            }
-        }
+        List<Integer> evenNumForEach = getEvenNumForEach(intNum);
+        System.out.println("Even numbers in the list using for each loop: " + evenNumForEach);
 
-        System.out.println("Even numbers in the list using while loop : ");
-        int i=0;
-        while (i < intNum.size()) {
-            int num= intNum.get(i);
-            if (num % 2 == 0) {
-                System.out.println(num);
-            }
-            i++;
-        }
+        List<Integer> evenNumWhile = getEvenNumWhile(intNum);
+        System.out.println("Even numbers in the list using while loop: " + evenNumWhile);
 
-        System.out.println("Even numbers in the list using for loop : ");
-        for(int j=0; j<intNum.size(); j++) {
-            int num= intNum.get(j);
-            if (num % 2 == 0) {
-                System.out.println(num);
-            }
-        }
+        List<Integer> evenNumFor = getEvenNumFor(intNum);
+        System.out.println("Even numbers in the list using for loop: " + evenNumFor);
 
-        System.out.println("Even numbers in the list using streams : ");
-        intNum.stream()
-                .filter(num -> num % 2 == 0)
-                .forEach(System.out::println);
-
+        List<Integer> evenNumStream = getEvenNumStream(intNum);
+        System.out.println("Even numbers in the list using streams: " + evenNumStream);
     }
 }
